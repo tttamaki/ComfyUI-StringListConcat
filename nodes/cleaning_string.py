025e650f02ab00a1df943271631756707851100a
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2026-08-16 10:35:06 +0900

    remove spaces before comma

--- nodes/cleaning_string.py
+++ nodes/cleaning_string.py
@@ -25,6 +25,7 @@ class CleaningString(io.ComfyNode):
     @classmethod
     def execute(cls, string: str) -> io.NodeOutput:
         result = re.sub(r"[\r\n]+", " ", string)
+        result = re.sub(r" +,", ",", result)
         result = re.sub(r",[ \t]+,", ",", result)
         result = re.sub(r" {2,}", " ", result)
         return io.NodeOutput(result)
