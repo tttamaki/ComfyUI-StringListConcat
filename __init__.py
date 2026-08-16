a02f449e668415beb25f6ef127e48dc097d5a3e8
Toru Tamaki <tamaki.toru@nitech.ac.jp>
2026-08-16 10:26:49 +0900

    fix init.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,18 @@
+from typing_extensions import override
+from comfy_api.latest import ComfyExtension, io
+
 from .nodes.cleaning_string import CleaningString
-from .nodes.string_list_concat import StringListConcat, comfy_entrypoint
+from .nodes.string_list_concat import StringListConcat
+
+
+class StringListConcatExtension(ComfyExtension):
+    @override
+    async def get_node_list(self) -> list[type[io.ComfyNode]]:
+        return [StringListConcat, CleaningString]
+
+
+async def comfy_entrypoint() -> StringListConcatExtension:
+    return StringListConcatExtension()
 
 __all__ = [
     "StringListConcat",
